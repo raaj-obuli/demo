1043a06b6fa0062c54cd6ce2a72c1f516033e71e
Demo <raaj-obuli@users.noreply.github.com>
2020-05-14 23:46:29 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,18 +1,20 @@
-$('.toggle').click(function(e) {
-  	e.preventDefault();
-  
-    var $this = $(this);
-  
-    if ($this.next().hasClass('show')) {
-        $this.parent().removeClass('show');
-        $this.next().removeClass('show');
-        $this.next().slideUp(350);
-    } else {
-        $this.parent().parent().find('li').removeClass('show');
-        $this.parent().parent().find('li .inner').removeClass('show');
-        $this.parent().parent().find('li .inner').slideUp(350);
-        $this.parent().toggleClass('show');
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(350);
-    }
+jQuery(function(){
+  $('.toggle').click(function(e) {
+      e.preventDefault();
+
+      var $this = $(this);
+
+      if ($this.next().hasClass('show')) {
+          $this.parent().removeClass('show');
+          $this.next().removeClass('show');
+          $this.next().slideUp(350);
+      } else {
+          $this.parent().parent().find('li').removeClass('show');
+          $this.parent().parent().find('li .inner').removeClass('show');
+          $this.parent().parent().find('li .inner').slideUp(350);
+          $this.parent().toggleClass('show');
+          $this.next().toggleClass('show');
+          $this.next().slideToggle(350);
+      }
+  });
 });
